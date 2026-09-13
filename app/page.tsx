3415651eb36ff246834c254710b5e3395622eb4f
Pujan-379 <Pujan.231723@gmail.com>
2026-09-13 21:42:03 +0545

    Swap ComposeBox for SharedNote on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import ComposeBox from "@/components/ComposeBox";
 import Feed from "@/components/Feed";
+import SharedNote from "@/components/SharedNote";
 export default function Home() {
   return (
     <>
-      <ComposeBox />
-      <Feed />
+      {/* <ComposeBox />
+      <Feed /> */}
+
+      <SharedNote />
     </>
   );
 }
